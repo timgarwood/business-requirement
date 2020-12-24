c2544690c5dd750c4cc496f21c82371c414384eb
unknown <timgarwood@gmail.com>
2020-12-24 15:24:57 -0500

    remove cors

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const jsonParser = bodyParser.json();
 const db = require('./spydb');
 const controller = require('./controllers/spy-controller.js');
 const fileUpload = require('express-fileupload');
 const path = require('path');
 
-app.use(cors());
 app.use(fileUpload({
     limits: { fileSize: 25 * 1024 * 1024 },
     createParentPath: true
